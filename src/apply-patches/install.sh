3a151e699e1968b6a27c5d2b4843ad015dbc77e3
Tim Lunn <tl@smlight.tech>
2024-04-11 17:16:26 +1000

    Apply Xosc tuning

--- src/apply-patches/install.sh
+++ src/apply-patches/install.sh
@@ -8,6 +8,7 @@ EMAIL="${GITEMAIL:-user@example.com}"
 NAME="${GITNAME:-anon user}"
 PATCHES_DIR="${PATCHESDIRECTORY:-/opt/sdk_patches}"
 SDK_DIR="${SDKDIRECTORY:-/opt/ti/ot-ti}"
+XOSC="${XOSCTUNING:-0xC1}"
 
 git config --global user.email \"${EMAIL}\"
 git config --global user.name \"${NAME}\"
@@ -15,5 +16,8 @@ git config --global user.name \"${NAME}\"
 echo "Applying patches from ${PATCHES_DIR}"
 cd ${SDK_DIR}
 git am --ignore-whitespace ${PATCHES_DIR}/*.patch
+
+echo "Setting Xosc Tuning to ${Xosc}"
+sed -Ei "s/xoscCapArrayDelta\s*=.*;/xoscCapArrayDelta = ${XOSC};/" src/openthread.syscfg
 
 echo "Patches applied successfully!"
