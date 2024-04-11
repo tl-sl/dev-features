a9f308293d17e6484c0ef282a987cf226c68bde7
Tim Lunn <tl@smlight.tech>
2024-04-11 13:10:12 +1000

    setup git identity

--- src/apply-patches/install.sh
+++ src/apply-patches/install.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-PATCHES_DIR="${DEVCONTAINER_FEATURE_OPTIONS_PATCHESDIRECTORY:-/workspaces/*/sdk_patches}"
-SDK_DIR="${DEVCONTAINER_FEATURE_OPTIONS_SDKDIRECTORY:-/opt/ti/ot-ti}"
+EMAIL="${GITEMAIL:-user@example.com}"
+NAME="${GITNAME:-anon user}"
+PATCHES_DIR="${PATCHESDIRECTORY:-/workspaces/*/sdk_patches}"
+SDK_DIR="${SDKDIRECTORY:-/opt/ti/ot-ti}"
+
+git config --global user.email \"${EMAIL}\"
+git config --global user.name \"${NAME}\"
+
 echo "Applying patches from ${PATCHES_DIR}"
 cd ${SDK_DIR}
 git am --ignore-whitespace ${PATCHES_DIR}/*.patch
